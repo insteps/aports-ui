ebe306a87e2392c50b21c72e693310a0dce0c9f3
V.Krishn <vkrishn4@gmail.com>
2016-07-07 04:35:53 +0530

    add depends in /package.html

--- public/js/aports.package.js
+++ public/js/aports.package.js
@@ -103,8 +103,14 @@
         }).appendTo( "body .container" );
 
         if(data) {
+            // get dependencies
+            var q_ = 'branch='+branch+'&arch='+arch+'';
+            url = config.api.baseurl+'/packages/id/'+d.id+'/depends'+'&'+q_;
+            asyncReq(url, 'callback', setDepends);
+            //alert(url);
+
             //set subpackages
-            var q_ = 'origin='+origin+'&branch='+branch+'&arch='+arch+'&repo='+repo+''
+            var q_ = 'origin='+origin+'&branch='+branch+'&arch='+arch+'&repo='+repo+'';
             url = config.api.baseurl+''+app.resource+'&'+q_;
             asyncReq(url, 'callback', setSubPackage);
         }
@@ -131,8 +137,22 @@
     }
 
     setDepends = function(data) {
-      alert(data);
-        
+        var p = new Poly9.URLParser(window.location);
+        var q = p.getQueryarray();
+        var items = []; var O = data.data[0]['attributes']['origin'];
+        var h = makeTblRow('', ['Dependencies '+'('+(data.meta.count-1)+')'], 'data', 'th', 'tr');
+        $.each( data.data, function( key, val ) {
+            if(val['attributes']['name'] !== q['name']) {
+              items.push(makeTblRow(val['attributes'], ['name'], 'data', 'td', 'tr'));
+            }
+        });
+        var tbl = "\n" + h + "\n" + items.join( "\n" );
+        $( "<table/>", {
+           "id": "depends",
+           "class": "packages",
+           //"class": "table table-striped table-bordered table-condensed",
+           html: tbl
+        }).appendTo( "body .container" );
     }
     
    _getPackage = function(url) {
